9916505d2c24ebefa2ed60bb0e28c89e30bf8cea
Christopher Phipps <Gforce@ChristophersMBP.home>
2014-09-10 15:29:01 -0400

    added a test class

---
+++ GGTest.h
@@ -0,0 +1,13 @@
+//
+//  GGTest.h
+//  Git Test
+//
+//  Created by Christopher Phipps on 9/10/14.
+//  Copyright (c) 2014 Christopher Phipps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GGTest : NSObject
+
+@end
---
+++ GGTest.m
@@ -0,0 +1,13 @@
+//
+//  GGTest.m
+//  Git Test
+//
+//  Created by Christopher Phipps on 9/10/14.
+//  Copyright (c) 2014 Christopher Phipps. All rights reserved.
+//
+
+#import "GGTest.h"
+
+@implementation GGTest
+
+@end
